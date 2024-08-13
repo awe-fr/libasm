a3b3caef559cd359a0d2a9eb5020c83f48dcc4cb
AWE <rajaouisahel@gmail.com>
2024-08-14 01:17:30 +0200

    git ignore

--- Sources/ft_write.s
+++ Sources/ft_write.s
@@ -5,16 +5,16 @@ extern __errno_location
 section .text
 	global ft_write
 	ft_write:
-		mov rax, 1						;init rax to call write
-		syscall							;call write
-		cmp rax, 0						;check if write fail
-		js error_manager				;if failed go to error func
-		ret								;return result of write
+		mov rax, 1							;init rax to call write
+		syscall								;call write
+		cmp rax, 0							;check if write fail
+		js error_manager					;if failed go to error func
+		ret									;return result of write
 
-	error_manager:
-		neg		rax						;invert rax and get error code
-		push	rax						;save rax into the stack
-		call __errno_location WRT ..plt	;get errno into rax
-		pop		qword[rax]				;get stack value (and cast) into errno
-		mov		rax, -1					;set rax to -1 because write failed
-		ret								;return rax
+		error_manager:
+			neg		rax						;invert rax and get error code
+			push	rax						;save rax into the stack
+			call __errno_location WRT ..plt	;get errno into rax
+			pop		qword[rax]				;get stack value (and cast) into errno
+			mov		rax, -1					;set rax to -1 because write failed
+			ret								;return rax
